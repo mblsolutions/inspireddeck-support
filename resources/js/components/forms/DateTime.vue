00e3224ea9041111486967ce56cb9dfec97c45d8
Lerouse <christian.innerbichler@gmail.com>
2019-10-09 10:59:02 +0100

    update datetime component

--- resources/js/components/forms/DateTime.vue
+++ resources/js/components/forms/DateTime.vue
@@ -2,16 +2,18 @@
 
     <div>
         <span v-if="error">
-            <datetime v-model="date" input-class="form-control form-control-error focus:border-brand-error-200" :title="title" :hidden-name="name" :placeholder="title" type="datetime"></datetime>
+            <datetime v-model="date" input-class="form-control form-control-error focus:border-brand-error-200" :title="title" :format="format" :hidden-name="name" :placeholder="title" :type="type" @close="selectedDateTime"></datetime>
         </span>
         <span v-else>
-            <datetime v-model="date" input-class="form-control" :title="title" :hidden-name="name" :format="format" :placeholder="title" type="datetime"></datetime>
+            <datetime v-model="date" input-class="form-control" :title="title" :hidden-name="name" :format="format" :placeholder="title" :type="type" @close="selectedDateTime"></datetime>
         </span>
     </div>
 
 </template>
 
 <script>
+    let moment = require('moment');
+
     export default {
         name: "DateTime.vue",
 
@@ -32,18 +34,45 @@
                 type: String,
                 default: 'y-LL-dd TT'
             },
+            type: {
+                type: String,
+                default: 'datetime'
+            },
             error: {
                 type: Boolean,
                 default: false
             }
         },
-
         data() {
             return {
                 date: null
             }
         },
+        methods: {
+            /**
+             * Selected DateTime
+             *
+             * @return void
+             */
+            selectedDateTime() {
+                let date = this.date;
 
+                if (this.type === 'date') {
+                    date = moment(this.date).format('YYYY-MM-DD');
+                } else if (this.type === 'time') {
+                    date = moment(this.date).format('HH:mm:ssZ');
+                }
+
+                let payload = {
+                    name: this.name,
+                    value: date
+                };
+
+                console.log(payload);
+
+                this.$emit('input-selected', payload);
+            }
+        },
         mounted() {
             let vm = this;
 
